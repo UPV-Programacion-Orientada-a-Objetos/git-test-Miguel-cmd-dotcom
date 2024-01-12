2037272dbdefd15748218ebafecdad77fc9e5ed1
Miguel Angel Hernandez Ramirez <2230309@upv.edu.mx>
2024-01-11 20:26:19 -0600

    se a agregado un archivo y se a modificado README.md

---
+++ OtroArchivo.java
@@ -0,0 +1,6 @@
+public class OtroArchivo {
+    public static void main(String[]args){
+        System.out.println("Hola a todos, este es otro intento del trabajo");
+        System.out.println("Eso fue todo, Adios :V");
+    }
+}
